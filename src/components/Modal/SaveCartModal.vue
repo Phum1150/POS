7359df2b9a8078b198d0ca8415863b7bf104a32c
Phum1150 <internDX@Jimthompson.com>
2026-09-09 14:11:04 +0700

    fix

--- src/components/Modal/SaveCartModal.vue
+++ src/components/Modal/SaveCartModal.vue
@@ -4,7 +4,7 @@ import { useCartStore } from '@/stores/cart'
 import BaseModal from './BaseModal.vue'
 
 const cart = useCartStore()
-const CartName = ref('')
+const cartName = ref('')
 
 const emit = defineEmits(['close', 'save'])
 
@@ -30,11 +30,11 @@ const save = (name) => {
     <BaseModal width="w-[20%]">
         <p class="text-xl font-semibold mb-2">Save Cart</p>
 
-        <input v-model="CartName" min="1" class="w-full p-2 border border-neutral-700 bg-neutral-900 rounded mb-2" placeholder="NewCart" />
+        <input v-model="cartName" min="1" class="w-full p-2 border border-neutral-700 bg-neutral-900 rounded mb-2" placeholder="NewCart" />
 
         <template #footer>
             <button @click="$emit('close')" class="px-4 py-2 border border-neutral-700 rounded hover:bg-neutral-800">Cancel</button>
-            <button @click="save(CartName)" class="bg-amber-500 text-black px-4 py-2 rounded hover:bg-amber-600">Save</button>
+            <button @click="save(cartName)" class="bg-amber-500 text-black px-4 py-2 rounded hover:bg-amber-600">Save</button>
         </template>
     </BaseModal>
 </template>
